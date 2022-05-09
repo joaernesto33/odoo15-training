85e57f9ccb4d2866369a1e2c5c9dbbe546a31a49
joaernesto33 <odoo@ampd1.odoo.com>
2022-05-09 18:04:46 +0000

    [ADD] Exercise Modules

---
+++ cooperative/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Cooperative Volunteers',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """
+        Academy Module to manage non-profit organisation:
+        -Volunteers
+        -Cooperative shops
+        -Local products
+    """,
+    
+    'author': 'Jokin',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
---
+++ space-mission/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space Mission',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """
+        Academy Module to manage space mission logistics:
+        -Planning
+        -Spaceship
+        -Space crew
+    """,
+    
+    'author': 'Jokin',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
