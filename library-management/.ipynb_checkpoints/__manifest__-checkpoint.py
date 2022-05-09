87017948090c4ef773ae651e4f83a98badd19a45
joaernesto33 <odoo@ampd1.odoo.com>
2022-05-09 18:04:46 +0000

    [ADD] Exercise Modules

---
+++ library-management/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Management',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """
+        Academy Module to manage customer checkings and organizing books:
+        -Books
+        -Customers
+        -Rentals
+    """,
+    
+    'author': 'Jokin',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
